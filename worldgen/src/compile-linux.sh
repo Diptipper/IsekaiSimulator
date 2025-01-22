0fd93be787aac979f96b932b5fdb6001e4e27e46
Diptipper <diptipper@gmail.com>
2025-01-22 19:17:01 +0900

    Delete worldgen/src directory

--- worldgen/src/compile-linux.sh
+++
@@ -1,7 +0,0 @@
-clear
-pyinstaller --onefile --add-data="assets/scripts:assets/scripts" main.py
-mkdir dist/assets
-cp -R assets/names dist/assets/names/
-rm -rf build
-rm main.spec
-mv dist build-linux
--- worldgen/src/compile-win.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cls
-pyinstaller --onefile --add-data="assets/scripts;assets/scripts" main.py
-mkdir dist\assets
-xcopy /E /I assets\names dist\assets\names\
-rmdir /S /Q build
-del main.spec
-ren dist build-win
